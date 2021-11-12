e1d0cd11509cab6333563b624b82827288b852bc
D[e]m <unknown.gmail.com>
2021-11-12 23:18:02 +0200

    first practice

---
+++ javascript.js
@@ -0,0 +1,20 @@
+"use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмах?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмах?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
