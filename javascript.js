1c0ffaaf6274a7cac5c2b1f658ee9980122a0507
D[e]m <unknown.gmail.com>
2021-11-15 22:31:00 +0200

    second

--- javascript.js
+++ javascript.js
@@ -9,12 +9,30 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмах?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмах?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
+
+      for (let i =0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмах?', ''),
+              b = prompt('На сколько оцените его?', '');
+              if (a != null && b != null && a !='' && b!= '' && a.length < 50) {
+                personalMovieDB.movies[a] = b;
+                console.log('Done');
+              } else {
+                  console.log('Error');
+                  i--;
+              }
+        
+
+    }
+
+    if (personalMovieDB.count < 10) {
+        console.log('Посмотренно довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классный зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
 
 console.log(personalMovieDB);
